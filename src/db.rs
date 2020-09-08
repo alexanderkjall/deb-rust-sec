6b3a6804df4fc2e00449d989949b40cd87d8e670
Alexander Kjäll <alexander.kjall@gmail.com>
2020-09-08 22:34:25 +0200

    initial commit

---
+++ src/db.rs
@@ -0,0 +1,84 @@
+use anyhow::Error;
+use postgres::{Client, NoTls};
+use serde::{Serialize, Deserialize};
+use serde_json;
+use std::fs;
+use std::path::PathBuf;
+use std::time::{Duration, SystemTime};
+
+
+const POSTGRES: &str = "postgresql://udd-mirror:udd-mirror@udd-mirror.debian.net/udd";
+const CACHE_EXPIRE: Duration = Duration::from_secs(90 * 60);
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CacheEntry {
+    pub from: SystemTime,
+    pub list: Vec<(String, String)>,
+}
+
+pub struct Connection {
+    client: postgres::Client,
+    cache_dir: PathBuf,
+}
+
+impl Connection {
+    pub fn new() -> Result<Connection, Error> {
+        let mut client = Client::connect(POSTGRES, NoTls)?;
+
+        let cache_dir = dirs::cache_dir().expect("cache directory not found")
+                                         .join("deb-rust-sec");
+
+        fs::create_dir_all(&cache_dir)?;
+
+        Ok(Connection {
+            client,
+            cache_dir,
+        })
+    }
+
+    fn cache_path(&self, release: &str) -> PathBuf {
+        self.cache_dir.join(release)
+    }
+
+    fn check_cache(&self, release: &str) -> Result<Option<Vec<(String, String)>>, Error> {
+        let path = self.cache_path(release);
+
+        if !path.exists() {
+            return Ok(None);
+        }
+
+        let buf = fs::read(path)?;
+        let cache: CacheEntry = serde_json::from_slice(&buf)?;
+
+        if SystemTime::now().duration_since(cache.from)? > CACHE_EXPIRE {
+            Ok(None)
+        } else {
+            Ok(Some(cache.list))
+        }
+    }
+
+    fn write_cache(&self, release: &str, result: &Vec<(String, String)>) -> Result<(), Error> {
+        let cache = CacheEntry {
+            from: SystemTime::now(),
+            list: result.clone(),
+        };
+        let buf = serde_json::to_vec(&cache)?;
+        fs::write(self.cache_path(release), &buf)?;
+        Ok(())
+    }
+
+    pub fn search(&mut self, release: &str) -> Result<Vec<(String, String)>, Error> {
+        let rows = self.client.query("select source::text, version::text from sources where bin like 'librust%' and release=$1;",
+                                        &[&release.to_string()])?;
+
+        let mut result: Vec<(String, String)> = vec![];
+        for row in &rows {
+            let source_name: String = row.get(0);
+            let debversion: String = row.get(1);
+
+            result.push((source_name, debversion))
+        }
+        self.write_cache(release, &result)?;
+        Ok(result)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use crate::db::Connection;
+
+use std::env;
+
+mod db;
+
+fn main() {
+    let dist = if env::args().len() > 1 {
+        env::args().skip(1).next().unwrap()
+    } else {
+        "sid".to_string()
+    };
+
+    let advisory_db_repo = rustsec::Repository::fetch(rustsec::repository::DEFAULT_URL, rustsec::Repository::default_path(), false)
+        .unwrap_or_else(|e| {
+            eprintln!("couldn't fetch advisory database: {}", e);
+            std::process::exit(1);
+        });
+
+    let database = rustsec::Database::load(&advisory_db_repo).unwrap_or_else(|e| {
+        eprintln!("error loading advisory database: {}", e);
+        std::process::exit(1);
+    });
+
+    let mut conn = Connection::new().unwrap();
+    let packages = conn.search(&dist).unwrap();
+
+    for vuln in database.iter() {
+        let vuln_crate = vuln.metadata.package.as_str().replace("_", "-");
+        for package in &packages {
+            let name = &package.0.replace("_", "-")[5..];
+            if vuln_crate == name {
+                let v:Vec<&str> = package.1.split("-").collect();
+                let is_version_affected = vuln.versions.is_vulnerable(&rustsec::version::Version::parse(v[0]).unwrap());
+                if is_version_affected {
+                    print!("{} : {}, ({}) {}", package.0, package.1, is_version_affected, &vuln.metadata.id.as_str());
+                    for id in &vuln.metadata.aliases {
+                        print!(" {}", id.as_str());
+                    }
+                    println!();
+                }
+            }
+        }
+    }
+}
